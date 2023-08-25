9959bbdd4f2b5dbed2bbe7a3b8729e7a2538b47f
aceday <markcediebuday@gmail.com>
2023-08-25 08:43:33 +0800

    Rectangle_Octo_Happiness

---
+++ rectangle_v1.py
@@ -0,0 +1,36 @@
+# <202280005@psu.palawan.edu.ph>
+# Function 1: Rectangle Octo Happiness
+# Coder: Mark Cedie Buday @ BSCS2-B1
+# Date Created: 2023-08-23
+''''
+Calculate the area of the rectangle using the length and width.
+
+Args:
+    length - The length of the rectangle
+    width - The height of the rectangle
+Return:
+    The calculated area of the rectangle
+'''
+def calculate_area(length:float,width: float)->float:
+    return length*width
+
+''''
+Calculate the perimeter of the rectangle using the length and width.
+
+Args:
+    length - The length of the rectangle
+    width - The height of the rectangle
+Return:
+    The calculated perimeter of the rectangle
+'''
+def calculate_perimeter(length:float,width:float)->float:
+    return 2 *(length+width)
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+area = calculate_area(length, width)
+perimeter = calculate_perimeter(length, width)
+
+print(f"Area of the rectangle: {area}")
+print(f"Perimeter of the rectangle: {perimeter}")
